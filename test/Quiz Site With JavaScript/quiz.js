01bb847ca42979067689aeab69710b7556b4d3ce
Peter-The-Great <56716150+Peter-The-Great@users.noreply.github.com>
2019-10-31 08:32:56 +0100

    look at this script yall
    Heb een soort van uitleg gegeven om het script uit te leggen.

--- test/Quiz Site With JavaScript/quiz.js
+++ test/Quiz Site With JavaScript/quiz.js
@@ -1,4 +1,4 @@
-(function() 
+(function() //creates the questions
  {
   var allQuestions = [{
     question: "The tree sends downroots from its branches to the soil is know as:",
@@ -41,13 +41,14 @@
     options: ["Graphite", "Silica", "Iron Oxide", "Diamond"],
     answer: 0
     }];
-  
+  //set the amount of points or questions you have taken at the start, how many options you have and how much space the quiz takes.
   var quesCounter = 0;
   var selectOptions = [];
   var quizSpace = $('#quiz');
     
   nextQuestion();
-    
+  //function of the next question. When clicked it wil check if a option has been selected if it does than it ads a point to the questions counter.
+ //it wil then go to the next question. If it does not find anything being entered than it wil alert (please select an option !).
   $('#next').click(function () 
     {
         chooseOption();
@@ -61,14 +62,14 @@
           nextQuestion();
         }
     });
-  
+  //goes to previous question. this is acoplished by decreasing the counter and setting the screen back to a previous question.
   $('#prev').click(function () 
     {
         chooseOption();
         quesCounter--;
         nextQuestion();
     });
-  
+  //creates all of the elements needed to display the quizez index inside the container and inside the div element quiz. And set al radio element in place
   function createElement(index) 
     {
         var element = $('<div>',{id: 'question'});
@@ -83,7 +84,7 @@
 
         return element;
     }
-  
+  //tels the index what the radiobuttons should contain if they are called and what kind of value they have in the index
   function radioButtons(index) 
     {
         var radioItems = $('<ul>');
@@ -98,12 +99,12 @@
         }
         return radioItems;
   }
-  
+  //makes a value out of a chosen question
   function chooseOption() 
     {
         selectOptions[quesCounter] = +$('input[name="answer"]:checked').val();
     }
-   
+   //when next function nextquestion happpends. the quizspace fades out of the screen the questions are removed and new questions are assigned.
   function nextQuestion() 
     {
         quizSpace.fadeOut(function() 
@@ -111,16 +112,16 @@
               $('#question').remove();
               if(quesCounter < allQuestions.length)
                 {
-                    var nextQuestion = createElement(quesCounter);
-                    quizSpace.append(nextQuestion).fadeIn();
+                    var nextQuestion = createElement(quesCounter);//creates the index to where the quescounter is at right now
+                    quizSpace.append(nextQuestion).fadeIn();//fades into the space it will be located to
                     if (!(isNaN(selectOptions[quesCounter]))) 
                     {
-                      $('input[value='+selectOptions[quesCounter]+']').prop('checked', true);
+                      $('input[value='+selectOptions[quesCounter]+']').prop('checked', true);//checkes if every thing is loaded correctly with the correct answer
                     }
-                    if(quesCounter === 1)
+                    if(quesCounter === 1)//if the quescounter is atleast one than show the prev button
                     {
                       $('#prev').show();
-                    } 
+                    } //else it wil hide the quescounter but it wil still show the next button no matter the quescounter
                     else if(quesCounter === 0)
                     {
                       $('#prev').hide();
@@ -129,7 +130,7 @@
                 }
               else 
                 {
-                    var scoreRslt = displayResult();
+                    var scoreRslt = displayResult();//if the results needed to be displayed, than it fades in and hides all the buttons.
                     quizSpace.append(scoreRslt).fadeIn();
                     $('#next').hide();
                     $('#prev').hide();
@@ -137,7 +138,8 @@
         });
     }
   
-  function displayResult() 
+  function displayResult() //display's results of the quiz and what answers you have chosen to choose. First it wil reset the variable correct to 0.
+ //then it will say for every option you have selected you might get a point depending if the answer is correct if so a point is added. it wil also display the folowing text
     {
         var score = $('<p>',{id: 'question'});
         var correct = 0;
@@ -150,5 +152,5 @@
         }
         score.append('You scored ' + correct + ' out of ' +allQuestions.length);
         return score;
-  }
+  }//shows the score you have at the end
 })();
